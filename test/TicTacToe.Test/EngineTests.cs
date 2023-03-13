10447369d825af1c96b7671f07a87fe1d1631a5c
Christophe Meudec <christophemeudec@msn.com>
2023-03-13 08:26:53 +0100

    Add tests

---
+++ test/TicTacToe.Test/EngineTests.cs
@@ -0,0 +1,72 @@
+namespace TicTacToe.Test;
+
+public class EngineTests
+{
+    [Fact]
+    public void Check_DefaultGame()
+    {
+        var game = new TicTacToe.Engine.TicTacToe();
+        Assert.IsAssignableFrom<TicTacToe.Engine.GameState.InProgress>(game.State);
+        
+        var inProgress = (TicTacToe.Engine.GameState.InProgress)game.State;
+        Assert.Empty(inProgress.History);
+        Assert.Equal(TicTacToe.Engine.Players.X, inProgress.CurrentPlayer);
+    }
+
+    [Fact]
+    public void Check_PlayerX_Win_Game()
+    {
+        var game = new TicTacToe.Engine.TicTacToe();
+
+        game.Play(Engine.GridPosition.BottomLeft);
+        game.Play(Engine.GridPosition.BottomCenter);
+        game.Play(Engine.GridPosition.Middle);
+        game.Play(Engine.GridPosition.TopLeft);
+        game.Play(Engine.GridPosition.TopRight);
+
+        Assert.IsAssignableFrom<TicTacToe.Engine.GameState.Finished>(game.State);
+        
+        var finished = (TicTacToe.Engine.GameState.Finished)game.State;
+        Assert.Equal(new[]{Engine.GridPosition.BottomLeft, Engine.GridPosition.BottomCenter, Engine.GridPosition.Middle, Engine.GridPosition.TopLeft, Engine.GridPosition.TopRight},finished.History);
+        Assert.IsAssignableFrom<TicTacToe.Engine.GameOutcome.Won>(finished.Outcome);
+        var outcome = (TicTacToe.Engine.GameOutcome.Won)finished.Outcome;
+        Assert.Equal(TicTacToe.Engine.Players.X, outcome.By);
+    }
+
+    [Fact]
+    public void Check_MoveError_PositionAlreadyUsed()
+    {
+        var game = new TicTacToe.Engine.TicTacToe();
+
+        game.Play(Engine.GridPosition.BottomLeft);
+
+        var validation = game.Play(Engine.GridPosition.BottomLeft);
+        Assert.NotNull(validation);
+        Assert.Equal(Engine.TicTacToe.MoveError.PositionAlreadyUsed, validation);
+    }
+
+    [Fact]
+    public void Check_Tie_Game()
+    {
+        var game = new TicTacToe.Engine.TicTacToe();
+
+        game.Play(Engine.GridPosition.BottomLeft);
+        game.Play(Engine.GridPosition.BottomCenter);
+        game.Play(Engine.GridPosition.BottomRight);
+        game.Play(Engine.GridPosition.CenterLeft);
+        game.Play(Engine.GridPosition.CenterRight);
+        game.Play(Engine.GridPosition.TopRight);
+        game.Play(Engine.GridPosition.TopLeft);
+        game.Play(Engine.GridPosition.Middle);
+        game.Play(Engine.GridPosition.TopCenter);
+
+        Assert.IsAssignableFrom<TicTacToe.Engine.GameState.Finished>(game.State);
+        var finished = (TicTacToe.Engine.GameState.Finished)game.State;
+        
+        Assert.IsAssignableFrom<TicTacToe.Engine.GameOutcome.Tie>(finished.Outcome);
+
+        var validation = game.Play(Engine.GridPosition.BottomLeft);
+        Assert.NotNull(validation);
+        Assert.Equal(Engine.TicTacToe.MoveError.AlreadyFinished, validation);
+    }
+}
